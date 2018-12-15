84cde1b2efb1f8c3c6f518ea4c885e127cb5756a
Kirill Polushin <kirill@polushin.org>
2018-12-15 19:52:26 +0700

    Add lab 12

--- second/lab10-13/conditions.c
+++ second/lab10-13/conditions.c
@@ -1,23 +1,68 @@
 
+#include <stdbool.h>
+#include <stdlib.h>
+
 #include "sync_common.h"
 #include "sync_prim.h"
 
-void sync_init() {}
+pthread_mutex_t mutex;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+bool child_next = false;
 
-void sync_parent_prelock() {}
+void sync_init() {
+  sync_errorcheck_mutex_init(&mutex);
+}
+
+void sync_parent_prelock() {
+  sync_mutex_lock(&mutex);
+}
 
 void sync_child_prelock() {}
 
-void sync_parent_postunlock() {}
+void sync_parent_postunlock() {
+  sync_mutex_unlock(&mutex);
+}
 
 void sync_child_postunlock() {}
 
-void sync_parent_lock() {}
+void sync_parent_lock() {
+  int error;
 
-void sync_parent_unlock() {}
+  sync_mutex_lock(&mutex);
+  // "While" because signal may be sent to self
+  while (child_next) {
+    if ((error = pthread_cond_wait(&cond, &mutex)) != 0) {
+      sync_error(error, "Cannot wait condition");
+      exit(error);
+    }
+  }
+}
 
-void sync_child_lock() {}
+void sync_parent_unlock() {
+  child_next = true;
+  pthread_cond_signal(&cond);
+  sync_mutex_unlock(&mutex);
+}
 
-void sync_child_unlock() {}
+void sync_child_lock() {
+  int error;
 
-void sync_destroy() {}
+  sync_mutex_lock(&mutex);
+  // "While" because signal may be sent to self
+  while (!child_next) {
+    if ((error = pthread_cond_wait(&cond, &mutex)) != 0) {
+      sync_error(error, "Cannot wait condition");
+      exit(error);
+    }
+  }
+}
+
+void sync_child_unlock() {
+  child_next = false;
+  pthread_cond_signal(&cond);
+  sync_mutex_unlock(&mutex);
+}
+
+void sync_destroy() {
+  sync_mutex_destroy(&mutex);
+}
